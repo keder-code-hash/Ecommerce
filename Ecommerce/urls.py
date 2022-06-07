9dda5bea4152012cb0fa772fff1f74d9183b39cb
testKeder <keder.nath.mallick.2019@cse.tict.edu.in>
2022-06-07 17:51:41 +0530

    requirements changed

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -20,6 +20,10 @@ from django.urls.conf import include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
 
     openapi.Info(
@@ -38,4 +42,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
