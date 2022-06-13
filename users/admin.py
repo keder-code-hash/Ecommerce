1d35eab973451558eb0da85dc6b109b78dae19ec
keder-code-hash <kedernath.mallick.tint022@gmail.com>
2022-06-13 13:03:33 +0530

    user admin pannel edited

--- users/admin.py
+++ users/admin.py
@@ -2,14 +2,23 @@ from django.contrib import admin
 
 from .models.Users import Users,UserAddressModel
 
-# from .models.Address import UsersAddressModel
-
-admin.site.register(Users)
+ 
 admin.site.register(UserAddressModel)
 
-# admin.site.register(UsersAddressModel)
+# edit user model admin panel
 
+class UserAdressInline(admin.StackedInline):
+    model = Users.address.through
+    extra = 1
+    verbose_name = "Add address to this user"
+
+class UserDeliveryAdressInline(admin.StackedInline):
+    model = Users.delivery_address.through
+    extra = 1
+    verbose_name = "Add delivery address to this user"
+    
 class CustomAdminPanel(admin.ModelAdmin):
-    def __init__(self, model, admin_site ) -> None:
-        super().__init__(Users, admin_site)
+    inlines = (UserAdressInline,UserDeliveryAdressInline,)
+    list_display = ('username','email',) 
 
+admin.site.register(Users,CustomAdminPanel)
